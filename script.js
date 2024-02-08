5d93ade8344e0d432f221442c05ca71c4aacdf68
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-08 17:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	let k={};
+  for(let i=0;i<arr.length();i++){
+	  for(let j=i+1;j<arr.length;j++){
+		  if(arr[i]+arr[j]===n&&n<0){
+			  k=(i,j);
+		  }
+	  }
+	  return k;
+  }
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
